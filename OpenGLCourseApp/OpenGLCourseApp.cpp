dce9a890f4db1f0909256ffbc715ffa4ed652cbd
William Luo <weilin.william.luo@gmail.com>
2019-04-27 22:32:37 -0700

    Lecture 7: [Coding] Uniform Variables

--- OpenGLCourseApp/OpenGLCourseApp.cpp
+++ OpenGLCourseApp/OpenGLCourseApp.cpp
@@ -10,7 +10,12 @@ using namespace std;
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformXMove;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxoffset = 0.7f;
+float triIncrement = 0.0005f;
 
 // Vertex shader
 static const char* vShader = "						\n\
@@ -18,9 +23,11 @@ static const char* vShader = "						\n\
 													\n\
 layout (location = 0) in vec3 pos;					\n\
 													\n\
+uniform float xMove;								\n\
+													\n\
 void main()											\n\
 {													\n\
-	gl_Position = vec4(0.4*pos.x, 0.4*pos.y, pos.z, 1.0);	\n\
+	gl_Position = vec4(0.4*pos.x + xMove, 0.4*pos.y, pos.z, 1.0);	\n\
 }";
 
 // Fragment shader
@@ -116,6 +123,8 @@ void CompileShaders()
 		cout << "Error validating program: " << eLog << endl;
 		return;
 	}
+
+	uniformXMove = glGetUniformLocation(shader, "xMove");
 }
 
 int main()
@@ -176,11 +185,25 @@ int main()
 		// Get and handle user input events
 		glfwPollEvents();
 
+		if (direction)
+		{
+			triOffset += triIncrement;
+		}
+		else {
+			triOffset -= triIncrement;
+		}
+
+		if (abs(triOffset) >= triMaxoffset) {
+			direction = !direction;
+		}
+
 		// clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
+
+		glUniform1f(uniformXMove, triOffset);
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
