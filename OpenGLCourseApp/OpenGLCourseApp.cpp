1d7dfd07fd94bbf71a2e32eb50bce2f1f5927f8d
William <william_luo2004@yahoo.com>
2019-04-30 16:46:26 -0700

    Lecture 9: [Coding] Transforming: Translation

--- OpenGLCourseApp/OpenGLCourseApp.cpp
+++ OpenGLCourseApp/OpenGLCourseApp.cpp
@@ -5,18 +5,21 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/mat4x4.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 using namespace std;
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader, uniformXMove;
+GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
 float triMaxoffset = 0.7f;
-float triIncrement = 0.0005f;
+float triIncrement = 0.005f;
 
 // Vertex shader
 static const char* vShader = "						\n\
@@ -24,11 +27,11 @@ static const char* vShader = "						\n\
 													\n\
 layout (location = 0) in vec3 pos;					\n\
 													\n\
-uniform float xMove;								\n\
+uniform mat4 model;									\n\
 													\n\
 void main()											\n\
 {													\n\
-	gl_Position = vec4(0.4*pos.x + xMove, 0.4*pos.y, pos.z, 1.0);	\n\
+	gl_Position = model * vec4(0.4*pos.x, 0.4*pos.y, pos.z, 1.0);	\n\
 }";
 
 // Fragment shader
@@ -125,7 +128,7 @@ void CompileShaders()
 		return;
 	}
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main()
@@ -204,7 +207,11 @@ int main()
 
 		glUseProgram(shader);
 
-		glUniform1f(uniformXMove, triOffset);
+		glm::mat4 model = glm::mat4(1.0);
+
+		model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
