3b92d93c14a6855b30360ca1f3cae3a1eb446b83
William Luo <weilin.william.luo@gmail.com>
2019-04-30 23:19:29 -0700

    Lecture 13: Interpolation

--- OpenGLCourseApp/OpenGLCourseApp.cpp
+++ OpenGLCourseApp/OpenGLCourseApp.cpp
@@ -34,22 +34,27 @@ static const char* vShader = "						\n\
 													\n\
 layout (location = 0) in vec3 pos;					\n\
 													\n\
+out vec4 vCol;										\n\
+													\n\
 uniform mat4 model;									\n\
 													\n\
 void main()											\n\
 {													\n\
 	gl_Position = model * vec4(pos, 1.0);			\n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);		\n\
 }";
 
 // Fragment shader
 static const char* fShader = "						\n\
 #version 330										\n\
 													\n\
+in vec4 vCol;										\n\
+													\n\
 out vec4 colour;									\n\
 													\n\
 void main()											\n\
 {													\n\
-	colour = vec4(0.0, 1.0, 0.0, 1.0);				\n\
+	colour = vCol;									\n\
 }";
 
 void CreateTriangle()
@@ -237,8 +242,9 @@ int main()
 		glm::mat4 model = glm::mat4(1.0);
 
 		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(currSize, 0.4f, 1.0f));
+		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 		
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
