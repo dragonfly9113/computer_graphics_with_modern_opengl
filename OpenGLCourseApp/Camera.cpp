fb79a1a2aa8bbeafe2c19361d5e9412a89689b76
William Luo <weilin.william.luo@gmail.com>
2019-05-07 09:26:43 -0700

    In progress...

---
+++ OpenGLCourseApp/Camera.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include "Camera.h"
+
+
+Camera::Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch, GLfloat startMoveSpeed, GLfloat startTurnSpeed)
+{
+	position = startPosition;
+	up = startUp;
+	yaw = startYaw;
+	pitch = startPitch;
+	front = glm::vec3(0.0f, 0.0f, -1.0f);
+
+	moveSpeed = startMoveSpeed;
+	turnSpeed = startTurnSpeed;
+
+	update();
+}
+
+void Camera::update()
+{
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front = glm::normalize(front);
+
+	right = glm::normalize(glm::cross(front, worldUp));
+	up = glm::normalize(glm::cross(right, front));
+}
+
+Camera::~Camera()
+{
+}
