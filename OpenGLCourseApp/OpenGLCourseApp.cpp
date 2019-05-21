49543204eae3b32a06326d0e049ae436ac9a4c41
Weilin Luo <william_luo2004@yahoo.com>
2019-05-21 16:59:35 -0700

    In progress...

--- OpenGLCourseApp/OpenGLCourseApp.cpp
+++ OpenGLCourseApp/OpenGLCourseApp.cpp
@@ -11,6 +11,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/string_cast.hpp>
+#include <assimp/Importer.hpp>
 
 #include "Window.h"
 #include "Mesh.h"
